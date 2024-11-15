c30f9cbf77887229ecd7a05c01520b3b823d2c62
COSTES SYLVAIN <sylvain.costes@audioptic.fr>
2024-11-15 19:24:28 +0100

    hh

--- src/layouts/components/Header.jsx
+++ src/layouts/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import {ModeToggle } from '../../hooks/mode-toggle.jsx';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
-    <div className="bg-gradient-to-r from-green-600 to-blue-600 text-white p-8">
+    <div className="bg-gradient-to-r  p-8">
         <header className="flex justify-between items-center">
+        <Link to="/test">
             <h1 className="text-5xl font-extrabold tracking-tight">Green Trip</h1>
-            <nav>
-                <ul className="flex space-x-8">
-                    <li><a href="Authentification" className="hover:underline transition duration-300">Connexion</a></li>
-                </ul>
-            </nav>
+            </Link>
+
+            <ModeToggle />
         </header>
     </div>
 );
