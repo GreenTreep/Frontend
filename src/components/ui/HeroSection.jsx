b8e6adfd1aca51cf8483edd95bdd01ea0176672a
COSTES SYLVAIN <sylvain.costes@audioptic.fr>
2024-11-15 19:24:28 +0100

    hh

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 
 const HeroSection = () => (
     <section className="relative">
-        <img src="https://example.com/your-image.jpg" alt="Header Image" className="w-full h-auto rounded-lg" />
-        <div className="absolute inset-0 flex items-center justify-center">
-            <h2 className="text-4xl font-semibold text-white"></h2>
-        </div>
+       
     </section>
 );
 
--- src/components/ui/MapSection.jsx
+++ src/components/ui/MapSection.jsx
@@ -1,48 +1,85 @@
 import React, { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 const MapSection = () => {
     const [departureCity, setDepartureCity] = useState('');
     const [arrivalCity, setArrivalCity] = useState('');
     const [departureDate, setDepartureDate] = useState('');
+    const [hikeType, setHikeType] = useState(''); // Nouvel état pour le type de randonnée
+
+    const handleHikeType = (type) => {
+        setHikeType(type);
+    };
 
     return (
-        <section id="map" className="p-10 bg-green-50 rounded-lg shadow-lg mt-6">
-            <h2 className="text-4xl font-semibold mb-4 text-gray-800">Planifiez votre Randonnée</h2>
-            <div className="flex flex-col md:flex-row items-center mb-6">
-                <div className="w-full md:w-1/2 mb-4 md:mr-4">
-                    <label className="block text-gray-700 mb-2">Ville de départ</label>
+        <section id="map" className="p-10 rounded-lg shadow-lg mt-6  dark:bg-gray-900">
+            <h2 className="text-4xl font-semibold mb-6 text-gray-800 dark:text-white">Planifiez votre Randonnée</h2>
+            <div className="flex flex-wrap items-center mb-8 space-x-4">
+                <div className="flex-1 min-w-[200px] mb-4">
+                    <label className="block mb-2 text-gray-700 dark:text-gray-200">Ville de départ</label>
                     <input
                         type="text"
-                        className="border rounded-lg w-full p-2 focus:outline-none focus:ring focus:ring-green-400"
+                        className="border border-gray-300 dark:border-gray-600 rounded-lg w-full p-2 focus:outline-none focus:ring-2 focus:ring-green-500 dark:focus:ring-blue-500 transition"
                         placeholder="Entrez votre ville de départ"
                         value={departureCity}
                         onChange={(e) => setDepartureCity(e.target.value)}
                     />
                 </div>
-                <div className="w-full md:w-1/2 mb-4 md:ml-4">
-                    <label className="block text-gray-700 mb-2">Ville d'arrivée</label>
+                <div className="flex-1 min-w-[200px] mb-4">
+                    <label className="block mb-2 text-gray-700 dark:text-gray-200">Ville d'arrivée</label>
                     <input
                         type="text"
-                        className="border rounded-lg w-full p-2 focus:outline-none focus:ring focus:ring-green-400"
+                        className="border border-gray-300 dark:border-gray-600 rounded-lg w-full p-2 focus:outline-none focus:ring-2 focus:ring-green-500 dark:focus:ring-blue-500 transition"
                         placeholder="Entrez votre ville d'arrivée"
                         value={arrivalCity}
                         onChange={(e) => setArrivalCity(e.target.value)}
                     />
                 </div>
+                <div className="flex-1 min-w-[200px] mb-4">
+                    <label className="block mb-2 text-gray-700 dark:text-gray-200">Date de départ</label>
+                    <input
+                        type="date"
+                        className="border border-gray-300 dark:border-gray-600 rounded-lg w-full p-2 focus:outline-none focus:ring-2 focus:ring-green-500 dark:focus:ring-blue-500 transition"
+                        value={departureDate}
+                        onChange={(e) => setDepartureDate(e.target.value)}
+                    />
+                </div>
+                <div className="flex-1 min-w-[200px] mb-4">
+                    <label className="block mb-2 text-green-700">Type de randonnée</label>
+                    <div className="flex space-x-4">
+                        <button
+                            className={`p-2 rounded-full border-2 ${
+                                hikeType === 'bike'
+                                    ? 'bg-green-500 text-white border-green-500'
+                                    : 'border-green-400 text-green-700 hover:bg-green-200'
+                            } transition`}
+                            onClick={() => handleHikeType('bike')}
+                            aria-label="Randonnée à vélo"
+                        >
+                            <Icon icon="material-symbols:directions-bike" width="24" height="24" />
+                        </button>
+                        <button
+                            className={`p-2 rounded-full border-2 ${
+                                hikeType === 'hiking'
+                                    ? 'bg-green-500 text-white border-green-500'
+                                    : 'border-green-400 text-green-700 hover:bg-green-200'
+                            } transition`}
+                            onClick={() => handleHikeType('hiking')}
+                            aria-label="Randonnée pédestre"
+                        >
+                            <Icon icon="material-symbols:hiking" width="24" height="24" />
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="mb-4">
-                <label className="block text-gray-700 mb-2">Date de départ</label>
-                <input
-                    type="date"
-                    className="border rounded-lg w-full p-2 focus:outline-none focus:ring focus:ring-green-400"
-                    value={departureDate}
-                    onChange={(e) => setDepartureDate(e.target.value)}
-                />
-            </div>
+
             <div className="flex justify-center mb-8">
-                <button className="bg-green-500 text-white py-3 px-6 rounded-lg hover:bg-green-700 transition duration-300 shadow-lg">Partir</button>
+                <button className="py-3 px-6 bg-green-600 text-white rounded-lg transition duration-300 shadow-lg hover:bg-green-700">
+                    Partir
+                </button>
             </div>
-            <div className="w-full h-64 rounded-lg overflow-hidden shadow-lg">
+
+            <div className="w-full rounded-lg overflow-hidden shadow-lg" style={{ height: '50vh' }}>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.3847488181107!2d-122.0838!3d37.42199999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fba1c3f4d83cf%3A0xa8b40b95b2db4b2!2sGoogleplex!5e0!3m2!1sen!2sus!4v1637578340524!5m2!1sen!2sus"
                     width="100%"
@@ -50,6 +87,7 @@ const MapSection = () => {
                     style={{ border: 0 }}
                     allowFullScreen
                     loading="lazy"
+                    title="Google Maps"
                 ></iframe>
             </div>
         </section>
