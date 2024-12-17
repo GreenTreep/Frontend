5165ed5d16650fccf8684f08c09e494c2426609e
RiadKCM <kcmriad@gmail.com>
2024-12-17 16:01:43 +0100

    feat : Add login signup design logic

--- src/layouts/BaseLayouts.jsx
+++ src/layouts/BaseLayouts.jsx
@@ -1,19 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import Background from './Background.jsx';
 
-const HorizontalLayout = () => {
+const BaseLayout = () => {
+  const location = useLocation(); 
+  const isLoginPage = location.pathname === '/login'; 
+  const isRegisterPage = location.pathname === '/register'; 
+
   return (
-    <div className={'h-screen overflow-auto '}>
-    <Background />
-      <Header />
-      <div className={' p-4 '}>
+    <div className="h-screen overflow-auto">
+      <Background />
+      {!isRegisterPage && !isLoginPage && <Header />} {/* Cache le Header si on est sur /login */}
+      <div>
         <Outlet />
       </div>
-      <Footer />
+      {!isRegisterPage && !isLoginPage && <Footer />}
     </div>
   );
 };
 
-export default HorizontalLayout;
+export default BaseLayout;
--- src/layouts/components/Header.jsx
+++ src/layouts/components/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import {ModeToggle } from '../../hooks/mode-toggle.jsx';
 import { Link } from 'react-router-dom';
+import { Button } from "@/components/ui/button";
+
 
 const Header = () => (
     <div className="bg-gradient-to-r  p-8">
         <header className="flex justify-between items-center">
-        <Link to="/test">
-            <h1 className="text-5xl font-extrabold tracking-tight">Green Trip</h1>
+
+
+            
+            <Link to="/test">
+            <p className="text-10l font-extrabold tracking-tight">Green Trip</p>
             </Link>
 
+            <div className="flex items-center">
+            <Link to="/login" className='px-2'>
+                <Button className='px-7 py-2'>Login</Button>
+            </Link>
             <ModeToggle />
+        </div>
         </header>
     </div>
 );
