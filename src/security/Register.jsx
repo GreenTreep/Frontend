5bb753ef08612550fb8bda89dd513fa8eb62570a
RiadKCM <kcmriad@gmail.com>
2024-12-18 03:56:06 +0100

    mm

--- src/security/Register.jsx
+++ src/security/Register.jsx
@@ -13,7 +13,7 @@ function Register() {
   return (
     <>
     <div 
-        className="flex items-center justify-center w-screen h-screen bg-cover bg-center bg-no-repeat bg-[url('/image_1.png')] scale-105"
+        className="flex items-center justify-center w-screen h-screen bg-cover bg-center bg-no-repeat bg-[url('/image_1.png')]"
     >
         <Card className="w-96 p-6 bg-opacity-80 bg-white rounded-lg shadow-md flex flex-col gap-4 opacity-0 animate-slidein300">
         {/* Header */}
