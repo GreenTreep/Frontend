d05229edadb9dcf053100bf2875a278a785db0cd
RiadKCM <kcmriad@gmail.com>
2024-12-18 14:43:04 +0100

    Authentication front

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,19 @@
-// Home.jsx
 import React from 'react';
 import HomeAdmin from './HomeAdmin.jsx';
 import HomeUser from './HomeUser.jsx';
 import HomeVisitor from './HomeVisitor.jsx';
 import Loading from './Loading.jsx';
-
+import { useAuth } from '@/security/auth/AuthContext';
 
 const Home = () => {
-  const user = null; 
-  const loading = false; 
+  const { user, loading } = useAuth();
+  console.log('[Home] Loading state:', loading, 'User state:', user);
 
   if (loading) {
-    
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   if (!user) {
-    
     return <HomeVisitor />;
   }
 
