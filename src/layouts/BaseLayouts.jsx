4f26f0474ced89f16d2dbbdae69b271657eeee40
COSTES SYLVAIN <sylvain.costes@audioptic.fr>
2024-12-19 20:37:16 +0100

    Fix MapLayout

--- src/layouts/BaseLayouts.jsx
+++ src/layouts/BaseLayouts.jsx
@@ -8,15 +8,16 @@ const BaseLayout = () => {
   const location = useLocation();
   const isLoginPage = location.pathname === '/login';
   const isRegisterPage = location.pathname === '/register';
+  const isMapBoxPage = location.pathname === '/mapbox';
 
   return (
     <div className="min-h-screen relative">
       <Background />
-      {!isRegisterPage && !isLoginPage && <Header />} {/* Affiche le Header sauf sur /login et /register */}
+      {!isRegisterPage && !isLoginPage && !isMapBoxPage && <Header />} {/* Affiche le Header sauf sur /login et /register */}
       <main className="relative z-10"> {/* Ajustez pt-20 selon la hauteur de votre Header */}
         <Outlet />
       </main>
-      {!isRegisterPage && !isLoginPage && <Footer />}
+      {!isRegisterPage && !isLoginPage && !isMapBoxPage && <Footer />}
     </div>
   );
 };
