497ca6a10f70ca46c62e9eb62e5c504db3df13c2
RiadKCM <kcmriad@gmail.com>
2025-01-16 12:17:34 +0100

    adding web socket

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+      // Création d'un alias pour `global` qui pointe vers `window`
+      global: 'window', // Utilise `window` au lieu de `global`
     },
   },
-})
+  define: {
+    // Ajout d'une définition globale pour "global"
+    global: 'window',
+  },
+  optimizeDeps: {
+    include: ['sockjs-client'], // Assure-toi que `sockjs-client` soit bien optimisé
+  },
+});
